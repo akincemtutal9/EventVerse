268b42651076bd36b837c6206ed11eecee5dcb9e
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-06-09 15:18:14 +0300

    feat: deactive gameobjects

---
+++ Assets/Project/Scripts/Network/DeactiveIfNotMine.cs
@@ -0,0 +1,14 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class DeactivateIfNotMine : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!photonView.IsMine)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
